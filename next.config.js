64da38f9a4d63863525f3c248504cb5e6bd44310
four-one-nine <owner@419.software>
2026-02-26 12:45:53 -0500

    More next.config.js changes

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = (phase, { defaultConfig }) => {
-  const isProd = phase === 'production';
-  return {
-    basePath: isProd ? '/power-calculator-collection' : '',
-    output: 'export',
-    trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
-  };
-=======
 const nextConfig = {
-  output: 'export',
   basePath: '/power-calculator-collection',
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
->>>>>>> 595905b607a1b20116f97005f6ba02373c7de134
 };
 
 module.exports = nextConfig;
